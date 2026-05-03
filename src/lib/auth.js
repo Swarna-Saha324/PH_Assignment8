4bda00146c107dff4f6e3e2d1a78ca1bbcf55577
Swarna-Saha324 <mumusaha85841@gmail.com>
2026-05-03 07:06:33 +0600

    Next_Public_ApI_URL

--- src/lib/auth.js
+++ src/lib/auth.js
@@ -3,14 +3,14 @@ import { MongoClient } from "mongodb";
 import { mongodbAdapter } from "better-auth/adapters/mongodb";
 
 const client = new MongoClient(process.env.MONGODB_URI);
-await client.connect();
 const db = client.db("BorrowingPlatform");
 
 export const auth = betterAuth({
   database: mongodbAdapter(db, {
-    // Optional: if you don't provide a client, database transactions won't be enabled.
     client
   }),
+  
+  baseURL: process.env.BETTER_AUTH_URL || process.env.NEXT_PUBLIC_APP_URL,
   emailAndPassword: { 
     enabled: true, 
   }, 
